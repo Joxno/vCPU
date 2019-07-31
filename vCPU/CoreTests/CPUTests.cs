76181f34b15f35b289137b3f97bf52bd21a00380
Joxno <sebastian.g.georghiou@gmail.com>
2019-07-31 22:15:07 +0200

    Initial Commit. Added first test for CPU object.

---
+++ vCPU/CoreTests/CPUTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Core.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace CoreTests
+{
+    [TestClass]
+    public class CPUTests
+    {
+        [TestMethod]
+        public void TickCPU()
+        {
+            var t_CPU = new CPU();
+            t_CPU.Tick();
+
+            var t_Counter = t_CPU.Ticks;
+            t_Counter.Should().Be(1, "We called Tick once.");
+        }
+    }
+}
---
+++ vCPU/CoreTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CoreTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CoreTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("38a492e7-ee2c-4265-bdbf-647b11af9cde")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
