399420e0d29d6808b276e98504c0aa669550cc5e
Joxno <sebastian.g.georghiou@gmail.com>
2019-08-01 14:13:25 +0200

    Started adding tests and implementation for MemoryBank

---
+++ vCPU/Core/Components/MemoryBank.cs
@@ -0,0 +1,21 @@
+﻿using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Components
+{
+    public class MemoryBank : IMemoryBank
+    {
+        private int m_Size;
+
+        public MemoryBank(int Size)
+        {
+            m_Size = Size;
+        }
+
+        public int Size => m_Size;
+    }
+}
---
+++ vCPU/CoreTests/MemoryBankTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Core.Components;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoreTests
+{
+    [TestClass]
+    public class MemoryBankTests
+    {
+        [TestMethod]
+        public void InitializeMemoryBank()
+        {
+            var t_Bank = new MemoryBank(100);
+        }
+
+        [TestMethod]
+        public void GetSizeOfMemoryBank()
+        {
+            var t_Bank = new MemoryBank(100);
+            var t_Size = t_Bank.Size;
+
+            t_Size.Should().Be(100, "We set MemoryBank to be able to hold 100 bytes in constructor.");
+        }
+    }
+}
